68fda5acbb6af7a89cbfa52194a229a423cd9819
Samir Katyar <samirkatiyar78685@gmail.com>
2022-04-16 19:05:25 +0530

    [feature/dashboard] * add map for data fast retrive. * add dynamic teacher and student in script. * remove extra variable from example.env

--- src/components/dashboard/dashboard.controller.js
+++ src/components/dashboard/dashboard.controller.js
@@ -9,12 +9,29 @@ const {
 } = require('../../services/error.response.helper');
 const dbDashboard = require('./dashboard.DAL');
 
+/**
+ * we have used here in memory map to store the data
+ * this is just for demo purpose
+ * we can use any other data store like redis, leveldb etc.
+ *
+ * once we have retrieved the data from the db. we will store it in map.
+ * next if request comes for the same data we will fetch it from the map
+ *
+ * when we add new data into db then we will remove the data from the map
+ */
+const analyticsMap = {};
+
 async function getHodDetailsBySubject(req, res) {
   try {
     const { subject } = req.query;
+    let result;
 
-    const result = await dbDashboard.getDetailsBySubject(subject);
-
+    if (analyticsMap[subject]) {
+      result = analyticsMap[subject];
+    } else {
+      result = await dbDashboard.getDetailsBySubject(subject);
+      analyticsMap[subject] = result;
+    }
     const response = fetchedSuccessfully({
       data: result,
       description: 'Analytics fetched successfully',
--- src/helper/database.helper.js
+++ src/helper/database.helper.js
@@ -6,76 +6,79 @@ const StudentResult = require('../components/student/studentResult.model');
 const log = require('../services/logger.service');
 
 const totalHod = parseInt(process.env.TOTAL_HOD, 10) || 5;
-const totalTeacherPerHod = parseInt(process.env.TOTAL_TEACHER_PER_HOD, 10) || 5;
-const totalStudentPerTeacher =
-  parseInt(process.env.TOTAL_STUDENT_PER_TEACHER, 10) || 35;
 
 async function initDatabase() {
   try {
-    log.info(`initializing Database first time`);
-    for (let hodIndex = 0; hodIndex < totalHod; hodIndex++) {
-      const nameIndex = Math.floor(Math.random() * names.length);
-      let hodDoc = {
-        name: names[nameIndex],
-        teachers: [],
-      };
-      hodDoc = new Hod(hodDoc);
-      hodDoc = await hodDoc.save();
-      for (
-        let teacherIndex = 0;
-        teacherIndex < totalTeacherPerHod;
-        teacherIndex++
-      ) {
-        const teacherNameIndex = Math.floor(Math.random() * names.length);
-        let teacherDoc = {
-          name: names[teacherNameIndex],
-          reportingHod: hodDoc._id,
-          students: [],
+    const hod = await Hod.findOne().exec();
+    if (!hod) {
+      log.info(`initializing Database first time`);
+      for (let hodIndex = 0; hodIndex < totalHod; hodIndex++) {
+        const nameIndex = Math.floor(Math.random() * names.length);
+        let hodDoc = {
+          name: names[nameIndex],
+          teachers: [],
         };
-        teacherDoc = new Teachers(teacherDoc);
-        teacherDoc = await teacherDoc.save();
-        hodDoc.teachers.push(teacherDoc._id);
+        hodDoc = new Hod(hodDoc);
+        hodDoc = await hodDoc.save();
+        const totalTeacherPerHod = Math.floor(Math.random() * (10 - 5 + 1)) + 5; // random number between 5 and 10
         for (
-          let studentIndex = 0;
-          studentIndex < totalStudentPerTeacher;
-          studentIndex++
+          let teacherIndex = 0;
+          teacherIndex < totalTeacherPerHod;
+          teacherIndex++
         ) {
-          const subjectOneIndex = Math.floor(Math.random() * subjects.length);
-          let subjectTwoIndex = Math.floor(Math.random() * subjects.length);
-          subjectTwoIndex =
-            subjectTwoIndex === subjectOneIndex
-              ? subjectTwoIndex === subjects.length - 1
-                ? subjectTwoIndex - 1
-                : subjectTwoIndex + 1
-              : subjectTwoIndex;
-          const subjectOneScore =
-            Math.floor(Math.random() * (100 - 30 + 1)) + 30;
-          const subjectTwoScore =
-            Math.floor(Math.random() * (100 - 30 + 1)) + 30;
-          const studentId = mongoose.Types.ObjectId();
-          let resultOne = {
-            studentId,
-            subject: subjects[subjectOneIndex],
-            teacher: teacherDoc._id,
-            mark: subjectOneScore,
+          const teacherNameIndex = Math.floor(Math.random() * names.length);
+          let teacherDoc = {
+            name: names[teacherNameIndex],
+            reportingHod: hodDoc._id,
+            students: [],
           };
-          let resultTwo = {
-            studentId,
-            subject: subjects[subjectTwoIndex],
-            teacher: teacherDoc._id,
-            mark: subjectTwoScore,
-          };
-          resultOne = new StudentResult(resultOne);
-          resultTwo = new StudentResult(resultTwo);
-          await resultOne.save();
-          await resultTwo.save();
-          teacherDoc.students.push(studentId);
+          teacherDoc = new Teachers(teacherDoc);
+          teacherDoc = await teacherDoc.save();
+          hodDoc.teachers.push(teacherDoc._id);
+          const totalStudentPerTeacher =
+            Math.floor(Math.random() * (50 - 20 + 1)) + 20; // random number between 20 and 50
+          for (
+            let studentIndex = 0;
+            studentIndex < totalStudentPerTeacher;
+            studentIndex++
+          ) {
+            const subjectOneIndex = Math.floor(Math.random() * subjects.length);
+            let subjectTwoIndex = Math.floor(Math.random() * subjects.length);
+            subjectTwoIndex =
+              subjectTwoIndex === subjectOneIndex
+                ? subjectTwoIndex === subjects.length - 1
+                  ? subjectTwoIndex - 1
+                  : subjectTwoIndex + 1
+                : subjectTwoIndex;
+            const subjectOneScore =
+              Math.floor(Math.random() * (100 - 30 + 1)) + 30;
+            const subjectTwoScore =
+              Math.floor(Math.random() * (100 - 30 + 1)) + 30;
+            const studentId = mongoose.Types.ObjectId();
+            let resultOne = {
+              studentId,
+              subject: subjects[subjectOneIndex],
+              teacher: teacherDoc._id,
+              mark: subjectOneScore,
+            };
+            let resultTwo = {
+              studentId,
+              subject: subjects[subjectTwoIndex],
+              teacher: teacherDoc._id,
+              mark: subjectTwoScore,
+            };
+            resultOne = new StudentResult(resultOne);
+            resultOne = await resultOne.save();
+            resultTwo = new StudentResult(resultTwo);
+            resultTwo = await resultTwo.save();
+            teacherDoc.students.push(studentId);
+          }
+          await teacherDoc.save();
         }
-        await teacherDoc.save();
+        await hodDoc.save();
       }
-      await hodDoc.save();
+      log.info(`Database Generated Successfully`);
     }
-    log.info(`Database Generated Successfully`);
   } catch (err) {
     log.error(`Error in generating Database ${err}`);
   }
